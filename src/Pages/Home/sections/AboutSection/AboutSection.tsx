a12dcac1cd448dcedff7f681ff5de3b8f22378db
Brenno Soares <brenno.soaresro@gmail.com>
2025-03-26 10:58:19 -0300

    Fixed AboutSection

---
+++ src/Pages/Home/sections/AboutSection/AboutSection.tsx
@@ -0,0 +1,79 @@
+import { Box, Card, Container, Grid, Typography, styled } from "@mui/material"
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import SchoolIcon from '@mui/icons-material/School';
+import AnimationComponent from "../../../../components/AnimationComponent/AnimationComponent";
+
+const AboutSection: React.FC = () => {
+
+    const StyledCard = styled(Card)(({ theme }) => ({
+        padding: "10px 10px",
+        textAlign: "center",
+        marginBottom: "10px",
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.light
+        }
+    }));
+
+    const skillsSet = [
+        "Javascript", "Typescript", "React", "Next", "Git", "HTML", "CSS", "Cypress", "AWS", "GCP", "Material UI", "Figma"
+    ]
+
+    return (
+        <>
+            <Container maxWidth="lg">
+                <Box id="about" pt={5} mb={3}>
+                    <Typography variant="h2" textAlign="center" >About me</Typography>
+                </Box>
+                <Grid container spacing={2} display="flex" justifyContent="center" pb={3}>
+                    <Grid item xs={9} md={2.5}>
+                        <AnimationComponent moveDirection="right">
+                            <StyledCard variant="outlined">
+                                <WorkspacePremiumIcon />
+                                <Typography textAlign="center" fontWeight={600}>Experience</Typography>
+                                                           <Typography textAlign="center">Frontend Development</Typography>
+                            </StyledCard>
+                        </AnimationComponent>
+                    </Grid>
+                    <Grid item xs={9} md={2.5}>
+                        <AnimationComponent moveDirection="left">
+                            <StyledCard variant="outlined">
+                                <SchoolIcon />
+                                <Typography textAlign="center" fontWeight={600}>Education</Typography>
+                                <Typography textAlign="center">2nd semester of Software Engineering</Typography>
+                            </StyledCard>
+                        </AnimationComponent>
+
+                    </Grid>
+                </Grid>
+                <Box pb={1}>
+                    <Typography>
+                        My name is Brenno, and I am a Software Engineering student currently in my second semester of college.
+                        I strive to enhance my skills in software development and problem-solving.
+                        I have a strong interest in programming, system architecture, and best coding practices.
+                        I explore new technologies to create efficient and scalable solutions.
+                        I believe in continuous learning and collaboration to drive projects forward.
+                        I have experience with C, Java, and TypeScript, as well as modern frameworks.
+                        I am always open to new challenges that contribute to my professional growth.
+                    </Typography>
+                </Box>
+                <hr />
+                <Box id="skills" pt={1} mb={3}>
+                    <Typography variant="h3" textAlign="center" fontWeight={300}>Skills</Typography>
+                </Box>
+                <Box mb={5}>
+                    <Grid container spacing={3} justifyContent="center">
+                        {skillsSet.map((skill, index) => (
+                            <Grid item key={index} xs={5} sm={4} md={2} lg={2}>
+                                <StyledCard variant="outlined">
+                                    {skill}
+                                </StyledCard>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Box>
+            </Container>
+        </>
+    )
+}
+
+export default AboutSection
